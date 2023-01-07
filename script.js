731650f5093b71b31aa2eec5ef6b9173cab00a23
Hungry_munk <abbaszadeharsam@gmail.com>
2023-01-08 09:27:52 +1100

    added reverstring function and calculator object with repsective tests

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-export function capitalise(string) {
+// capitalises the first letter of a string if possible
+export const capitalise = string => {
     if (typeof string !== "string") return "not a string";
     return string[0].toUpperCase() + string.slice(1);
-}
+};
+
+//  reverses the order of a string
+export const reverseString = string => {
+    if (typeof string !== "string") return "not a string";
+    let newString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        newString += string[i];
+    }
+    return newString;
+};
+
+// calculator object
+export const calculator = (() => {
+    const add = (num1, num2) => {
+        return num1 + num2;
+    };
+
+    const subtract = (num1, num2) => {
+        return num1 - num2;
+    };
+
+    const divide = (num1, num2) => {
+        return num1 / num2;
+    };
+
+    const multiply = (num1, num2) => {
+        return num1 * num2;
+    };
+
+    return { add, subtract, divide, multiply };
+})();
+
+const cesarCipher = (string, shift) => {};
--- script.test.js
+++ script.test.js
@@ -1,11 +1,67 @@
-import { capitalise } from "./script.js";
+import { capitalise, reverseString, calculator } from "./script.js";
 
-test('takes the "arsam" and capitalises the fist letter returning "Arsam"', () => {
-    expect(capitalise("arsam")).toBe("Arsam");
+// capitalise string tests
+describe("capitalise", () => {
+    test('takes the "arsam" and capitalises the fist letter returning "Arsam"', () => {
+        expect(capitalise("arsam")).toBe("Arsam");
+    });
+    test('takes the "cHEESE" and capitalises the fist letter returning "CHEESE"', () => {
+        expect(capitalise("cHEESE")).toBe("CHEESE");
+    });
+    test('doesnt allow non string arguemnts such as 1 returning "not a string"', () => {
+        expect(capitalise(1)).toBe("not a string");
+    });
 });
-test('takes the "cHEESE" and capitalises the fist letter returning "CHEESE"', () => {
-    expect(capitalise("cHEESE")).toBe("CHEESE");
+
+// reverstring tests
+describe("reverseString", () => {
+    test("takes the string Arsam and reverses it returning masrA", () => {
+        expect(reverseString("Arsam")).toBe("masrA");
+    });
+    test("takes the string Abbaszadeh and reverses it returning hedazsabbA", () => {
+        expect(reverseString("Abbaszadeh")).toBe("hedazsabbA");
+    });
+    test('doesnt allow non string arguemnts such as 1 returning "not a string"', () => {
+        expect(reverseString(1)).toBe("not a string");
+    });
 });
-test('doesnt allow non string arguemnts such as 1 returning "not a string"', () => {
-    expect(capitalise(1)).toBe("not a string");
+
+// calculator object
+describe("calculator object", () => {
+    // add method
+    describe("add method", () => {
+        test("takes 1 and 2 as args returning 3", () => {
+            expect(calculator.add(1, 2)).toBe(3);
+        });
+        test("takes 4 and 2 as args returning 6", () => {
+            expect(calculator.add(4, 2)).toBe(6);
+        });
+    });
+    // subtract method
+    describe("subtract method", () => {
+        test("takes 8 and 2 as args returning 6", () => {
+            expect(calculator.subtract(8, 2)).toBe(6);
+        });
+        test("takes 2 and 4 as args returning -2", () => {
+            expect(calculator.subtract(2, 4)).toBe(-2);
+        });
+    });
+    // divide method
+    describe("divide method", () => {
+        test("takes 8 and 2 as args returning 4", () => {
+            expect(calculator.divide(8, 2)).toBe(4);
+        });
+        test("takes 2 and 4 as args returning 0.5", () => {
+            expect(calculator.divide(2, 4)).toBe(0.5);
+        });
+    });
+    // multiply method
+    describe("multiply method", () => {
+        test("takes 8 and 2 as args returning 16", () => {
+            expect(calculator.multiply(8, 2)).toBe(16);
+        });
+        test("takes 2 and 4 as args returning 8", () => {
+            expect(calculator.multiply(2, 4)).toBe(8);
+        });
+    });
 });
